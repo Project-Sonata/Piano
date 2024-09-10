8578fb9cc885fb79dcd33c701934e90945d62b87
odeyalo <theprogrammerinfuture@gmail.com>
2024-09-10 17:55:41 +0300

    Fulfilled DTO with required fields, marked fields with NotNull annotation

--- src/main/java/com/odeyalo/sonata/piano/api/dto/RegistrationFormDto.java
+++ src/main/java/com/odeyalo/sonata/piano/api/dto/RegistrationFormDto.java
@@ -3,9 +3,11 @@ package com.odeyalo.sonata.piano.api.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.odeyalo.sonata.piano.api.model.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 
@@ -14,9 +16,16 @@ import java.time.LocalDate;
 @AllArgsConstructor(onConstructor_ = {@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)})
 public class RegistrationFormDto {
     @JsonProperty("email")
+    @NotNull
     String email;
     @JsonProperty("password")
+    @NotNull
     String password;
     @JsonProperty("birthdate")
+    @NotNull
     LocalDate birthdate;
+    @NotNull
+    Gender gender;
+    @JsonProperty("enable_notification")
+    boolean enableNotification;
 }
---
+++ src/main/java/com/odeyalo/sonata/piano/api/model/Gender.java
@@ -0,0 +1,7 @@
+package com.odeyalo.sonata.piano.api.model;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NONE
+}
