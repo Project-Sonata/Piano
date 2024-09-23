e9f8762c7d8593d72e78499ef41327c67e0faf9c
odeyalo <theprogrammerinfuture@gmail.com>
2024-09-23 19:53:24 +0300

    Add ConfirmationCode that used to confirm the email, add ConfirmationCodeService to work with ConfirmationCode, add InMemoryConfirmationCodeService implementation

---
+++ src/main/java/com/odeyalo/sonata/piano/service/confirmation/InMemoryConfirmationCodeService.java
@@ -0,0 +1,55 @@
+package com.odeyalo.sonata.piano.service.confirmation;
+
+import com.odeyalo.sonata.piano.exception.InvalidConfirmationCodeException;
+import com.odeyalo.sonata.piano.model.User;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public final class InMemoryConfirmationCodeService implements ConfirmationCodeService {
+    private final Map<String, ConfirmationCode> confirmationCodes;
+    private final ConfirmationCodeFactory confirmationCodeFactory;
+
+    public InMemoryConfirmationCodeService(final ConfirmationCodeFactory confirmationCodeFactory) {
+        this.confirmationCodeFactory = confirmationCodeFactory;
+        this.confirmationCodes = new ConcurrentHashMap<>();
+    }
+
+    public InMemoryConfirmationCodeService(List<ConfirmationCode> confirmationCodes, final ConfirmationCodeFactory confirmationCodeFactory) {
+
+        this.confirmationCodes = confirmationCodes.stream().collect(Collectors.toMap(
+                ConfirmationCode::value,
+                Function.identity()
+        ));
+        this.confirmationCodeFactory = confirmationCodeFactory;
+    }
+
+    @Override
+    @NotNull
+    public Mono<ConfirmationCode> newConfirmationCodeFor(final @NotNull User user) {
+        return Mono.fromCallable(() -> {
+            ConfirmationCode code = confirmationCodeFactory.newConfirmationCodeFor(user);
+            confirmationCodes.put(code.value(), code);
+            return code;
+        });
+    }
+
+    @Override
+    @NotNull
+    public Mono<ConfirmationCode> loadConfirmationCodeByValue(final @NotNull String value) {
+        return Mono.justOrEmpty(confirmationCodes.get(value))
+                .flatMap(confirmationCode -> {
+                    if ( confirmationCode.isExpired() ) {
+                        return Mono.error(new InvalidConfirmationCodeException("Confirmation code is expired and can't be used!"));
+                    }
+                    return Mono.just(confirmationCode);
+                });
+    }
+}
