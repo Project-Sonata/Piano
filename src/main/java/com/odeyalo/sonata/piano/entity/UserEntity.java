c14190fb9b7453002aecf775aa00b6c43cd799c9
odeyalo <theprogrammerinfuture@gmail.com>
2025-07-12 16:33:53 +0300

    Add user table

--- src/main/java/com/odeyalo/sonata/piano/entity/UserEntity.java
+++ src/main/java/com/odeyalo/sonata/piano/entity/UserEntity.java
@@ -1,7 +1,59 @@
 package com.odeyalo.sonata.piano.entity;
 
+import com.odeyalo.sonata.piano.model.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+import lombok.With;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.time.LocalDate;
+
+@Value
+@Builder
 @Table("users")
-public final class UserEntity {
+@With
+public class UserEntity {
+    @Id
+    Long id;
+    @NotNull
+    @Column("external_id")
+    String externalId;
+    @NotNull
+    @Column("email")
+    String email;
+    @NotNull
+    @Column("password")
+    String password;
+    @NotNull
+    @Column("gender")
+    Gender gender;
+    @Column("is_activated")
+    boolean activated;
+    @Column("is_email_confirmed")
+    boolean emailConfirmed;
+    @NotNull
+    @Column("birthdate")
+    LocalDate birthdate;
+
+    @PersistenceCreator
+    public UserEntity(final Long id, @NotNull final String externalId, @NotNull final String email, @NotNull final String password, @NotNull final Gender gender, final boolean activated, final boolean emailConfirmed, @NotNull final LocalDate birthdate) {
+        this.id = id;
+        this.externalId = externalId;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.activated = activated;
+        this.emailConfirmed = emailConfirmed;
+        this.birthdate = birthdate;
+    }
+
+    @NotNull
+    public static UserEntityBuilder create() {
+        return builder();
+    }
 }
--- src/test/java/com/odeyalo/sonata/piano/repository/UserRepositoryTest.java
+++ src/test/java/com/odeyalo/sonata/piano/repository/UserRepositoryTest.java
@@ -1,23 +1,51 @@
 package com.odeyalo.sonata.piano.repository;
 
 
+import com.odeyalo.sonata.piano.entity.UserEntity;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import reactor.test.StepVerifier;
 import testing.base.AbstractIntegrationTest;
+import testing.faker.UserEntityFaker;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataR2dbcTest
 class UserRepositoryTest extends AbstractIntegrationTest {
 
     @Autowired
     UserRepository testable;
+    private final Logger logger = LoggerFactory.getLogger(UserRepositoryTest.class);
 
     @Test
-    void shouldNotFail() {
-        testable.findAll()
-                .as(StepVerifier::create)
-                .expectNextCount(0)
-                .verifyComplete();
+    void shouldFindAllSavedUsers() {
+        // given
+        final UserEntity user1 = UserEntityFaker.newUser().get();
+        final UserEntity user2 = UserEntityFaker.newUser().get();
+
+        logger.info("About to save 2 users: {}, {}", user1, user2);
+
+        final List<UserEntity> savedUsers = insertUsers(user1, user2);
+
+        // when
+        final List<UserEntity> foundUsers = testable.findAll().collectList().block();
+
+        // then
+        assertThat(foundUsers).containsAll(savedUsers);
+    }
+
+    @NotNull
+    private List<UserEntity> insertUsers(@NotNull final UserEntity... users) {
+        return Objects.requireNonNull(
+                testable.saveAll(List.of(users)).collectList().block(),
+                () -> "Error occurred during saving to database"
+        );
     }
 }
