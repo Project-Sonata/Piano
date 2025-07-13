26649bc2e0f08c87af1f24bed2be3570de548bf3
odeyalo <theprogrammerinfuture@gmail.com>
2025-07-13 14:56:47 +0300

    Check that files were added

--- ci/changelog.py
+++ ci/changelog.py
@@ -1,21 +1,29 @@
 import sys
 
-def changelog(changed_changelog_files):
+def changelog(cli_args):
     print("Hello!")
-    print("These yaml files were added: ")
-    print(changed_changelog_files)
+    if len(args) == 0:
+        print("Changelog file was not created!")
+        sys.exit(1)
 
-def check_files_not_modified(all_changed_files, added_files):
+    added_files = args[0]
+    print("These yaml files were added: ")
+
+    print(added_files)
+
+def check_files_not_modified(cli_args):
+
+    all_changed_files, added_files = cli_args[0], cli_args[1]
     print("Checking modified files...")
     pass
 
 if __name__ == '__main__':
-    print("These arguments were supplied: ")
-    print(sys.argv)
     args = sys.argv[1:]
+    print(f"Running a changelog validation with arguments {args}")
+
     if args[0] == '-m':
-        check_files_not_modified(args[1], args[2])
+        check_files_not_modified(args[1:])
     if args[0] == '-a':
-        changelog(args[1])
+        changelog(args[1:])
 
 
