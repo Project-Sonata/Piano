015d3c088905aa24a1875cf2c42acc53a0120d22
odeyalo <theprogrammerinfuture@gmail.com>
2025-07-13 14:41:20 +0300

    Add check that no old files were deleted and only new was added

--- ci/changelog.py
+++ ci/changelog.py
@@ -1,11 +1,20 @@
 import sys
 
-def changelog(changed_files):
+def changelog(changed_changelog_files):
     print("Hello!")
-    print("There files were changed: ")
-    print(changed_files)
+    print("These yaml files were added: ")
+    print(changed_changelog_files)
 
+def check_files_not_modified(all_changed_files, added_files):
+    print("Checking modified files...")
+    pass
 
 if __name__ == '__main__':
 
-    changelog(sys.argv[1:])
+    args = sys.argv[1:]
+    if args[0] == '-m':
+        check_files_not_modified(args[1], args[2])
+    if args[0] == '-a':
+        changelog(args[1])
+
+
