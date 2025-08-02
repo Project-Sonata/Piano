3e7f8aa96dc842ebe0bdc3fdb8580231d2827141
odeyalo <theprogrammerinfuture@gmail.com>
2025-08-02 17:16:01 +0300

    Use SecureRandom

--- src/main/java/com/odeyalo/sonata/piano/service/confirmation/SimpleConfirmationCodeFactory.java
+++ src/main/java/com/odeyalo/sonata/piano/service/confirmation/SimpleConfirmationCodeFactory.java
@@ -2,8 +2,10 @@ package com.odeyalo.sonata.piano.service.confirmation;
 
 import com.odeyalo.sonata.piano.model.User;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.security.SecureRandom;
 import java.time.Instant;
 
 /**
@@ -14,8 +16,11 @@ public final class SimpleConfirmationCodeFactory implements ConfirmationCodeFact
     @Override
     @NotNull
     public ConfirmationCode newConfirmationCodeFor(@NotNull final User user) {
+        SecureRandom random = new SecureRandom();
+        int number = random.nextInt(900000) + 100000;
+
         return new ConfirmationCode(
-                RandomStringUtils.randomNumeric(6),
+                String.valueOf(number),
                 Instant.now(),
                 Instant.now().plusSeconds(360),
                 user
