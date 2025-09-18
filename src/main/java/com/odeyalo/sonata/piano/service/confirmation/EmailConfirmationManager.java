ca0bca2a78aa5752a59b35c381638bada149c8b8
odeyalo <theprogrammerinfuture@gmail.com>
2025-09-18 15:31:16 +0300

    Remove unused imports

--- src/main/java/com/odeyalo/sonata/piano/service/confirmation/EmailConfirmationManager.java
+++ src/main/java/com/odeyalo/sonata/piano/service/confirmation/EmailConfirmationManager.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
